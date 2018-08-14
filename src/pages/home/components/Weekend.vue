d9bc41e6ea8d308d3f9dd823b692c64d9f3e3a84
blucexie <649453836@qq.com>
2018-08-14 11:57:02 +0800

    travel

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,48 @@
+<template>
+   <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of list" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+   </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  props: {
+    list: Array
+  }
+}
+</script>
+<style lang ="stylus" scoped>
+@import '~styles/mixins.styl';
+    .title
+        line-height .8rem
+        background #eee
+        text-indent .2rem
+    .itempimg-wrapper
+        overflow hidden
+        height 0
+        padding-bottom 33.9%
+    .item-img
+        width 100%
+    .item-info
+        padding .1rem
+        .item-title
+            ellipsis()
+            line-height .54rem
+            font-size .32rem
+        .item-desc
+            ellipsis()
+            line-height .4rem
+            color #cccccc
+
+</style>
